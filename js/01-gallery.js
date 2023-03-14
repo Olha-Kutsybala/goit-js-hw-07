5b5dfa0053fdbec5e230173494343f6ea84267ff
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-03-14 17:37:00 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemGallery = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", itemGallery);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function createGalleryItem(items) {
+    return galleryItems.map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+}
+
+function onGalleryContainerClick(e) {
+    if (!e.target.classlist.contains('gallery__link')) {
+        return
+    }
+    return galleryItems.original;
+}
+  
